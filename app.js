e3aff2edafd43a73719db8b3395ef6fe74d658b9
shoaib3456 <63105075+shoaib3456@users.noreply.github.com>
2020-07-03 17:57:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let  menu = document.getElementById('menu')
+ 
+function ham_func(){
+    if(menu.style.display=="flex"){
+        menu.style.display="none"
+    }
+    else{
+        menu.style.display="flex"
+    }
+ }
